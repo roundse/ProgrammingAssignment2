f715b600260477b169b7db6731ceebfd43bc0655
Emily Rounds <elrounds@gmail.com>
2018-10-13 15:58:00 -0700

    First iteration of cache inverse functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Initialize inverse value to null.
+        inv <- NULL
+        
+        set <- function(y) {
+          x <<- y
+          inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- solve()
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Check if inverse is stored.
+        if (!x$getinverse) {
+                ## Check to see if x is a matrix and if yes,
+                ## return a matrix that is the inverse of 'x'
+                if (is.matrix(x)) {
+                        inv <- solve(x)
+                }
+        }
+                
 }
